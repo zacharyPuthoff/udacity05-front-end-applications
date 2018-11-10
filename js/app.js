83069ee12af8cb044ef0976d1ac3fdb70d43f927
zak <zak.puthoff@gmail.com>
2018-11-10 03:02:58 -0600

    Streamlines JS code with better DOM selectors

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 // an object class with all relevant data for each picture object
 class Cat {
-  constructor(name, position) {
-    this.position = position;
-    this.name = name;
+  constructor(catName, picID) {
+    this.picID = picID;
+    this.catName = catName;
     this.clicks = 0;
   }
   updateHTML() {
-    document.querySelectorAll('.count')[this.position].innerText = `Clicks: ${this.clicks}`;
+    document.querySelector(`#${this.picID} ~ span.count`).innerText = `Clicks: ${this.clicks}`;
   }
 }
 
@@ -16,26 +16,26 @@ class Cat {
 const catPics = document.querySelectorAll('.cat-pics');
 let theCatList = [];
 
-/* this code cycles through all the catPics and pulls out the name & position 
-   (the numbering starts with 0), then creates a new cat picture object and
+/* this code cycles through all the catPics and pulls out the name & ID 
+   for each cat picture, then creates a new cat picture object and
    assigns those values to it, and then pushes it into our array of  
    cat picture objects which we'll reference in a moment
 */ 
 for (let eachCat of catPics) {
-  let theName = eachCat.querySelector('.name').innerText;
-  let thePosition = eachCat.querySelector('img').id;
-  let newCat = new Cat(theName, thePosition);
+  let thePicName = eachCat.querySelector('.name').innerText;
+  let thePicID = eachCat.querySelector('img').id;
+  let newCat = new Cat(thePicName, thePicID);
 
   theCatList.push(newCat);
 }
 
 /* this is the event listener that records the clicks a user performs; 
    the first part checks to be sure that an image was clicked on, and if
-   not then it exits; otherwise, the id of the clicked image is pulled, and 
-   i've coded the id for each pipcture to correspond to it's position in the
-   lineup of cat pictures; we then use that info to increment the clicks
-   property in that particular cat picture object, and then use the built in
-   method to change the HTML on the page so the click count display properly 
+   not then it exits; otherwise, the id of the clicked image is pulled, and
+   modified to reflect it's index in the array of cat picture objects; I then 
+   use that info to increment the clicks property in that particular cat picture
+   object, and then use the built in method to change the HTML on the page so 
+   the click count displays accurately
 */ 
 document.querySelector('#the-click-area').addEventListener('click', (event) => {
   let clickedElement = event.target.nodeName.toLowerCase();
@@ -44,8 +44,8 @@ document.querySelector('#the-click-area').addEventListener('click', (event) => {
     return;
   }
 
-  let picID = event.target.id;
+  let catArrayIndex = Number(event.target.id.slice(-2)) - 1;
 
-  theCatList[picID].clicks++;
-  theCatList[picID].updateHTML();
+  theCatList[catArrayIndex].clicks++;
+  theCatList[catArrayIndex].updateHTML();
 });
