60d7338744af0da74e31ccc83c8590bfa9a228c5
zak <zak.puthoff@gmail.com>
2018-11-15 14:49:36 -0600

    Adds the admin button and functionality to the project
    This allows us to change the values of the current cat to whatever we like; as this information is all done client side, a form
    was not used, but just straight up JS

--- js/mvoapp.js
+++ js/mvoapp.js
@@ -13,8 +13,11 @@ const octopus = {
   init() {
     catListView.createList();
     currentCatView.render(true);
-    this.listenForListClicks();
-    this.listenForCatClicks();
+    this.listClickListener();
+    this.currentCatClickListener();
+    this.adminButtonListener();
+    this.saveButtonListener();
+    this.cancelButtonListener();
   },
   getCurrentCat() {
     return model.currentCat;
@@ -22,8 +25,8 @@ const octopus = {
   getCats() {
     return model.cats;
   },
-  listenForListClicks() {
-    document.querySelector('aside').addEventListener('click', (event) => {
+  listClickListener() {
+    document.querySelector('#cat-list').addEventListener('click', (event) => {
       let listItemClicked = event.target;
 
       if (listItemClicked.nodeName.toLowerCase() !== 'p') { return; }
@@ -33,14 +36,34 @@ const octopus = {
       currentCatView.render();
     });
   },
-  listenForCatClicks() {
-    document.querySelector('#the-cat-pic').addEventListener('click', (event) => {
-      let clickedCat = event.target;
-
-      if (clickedCat.nodeName.toLowerCase() !== 'img') { return; }
-
+  currentCatClickListener() {
+    document.querySelector('#cat-display').addEventListener('click', (event) => {
+      if (event.target.id !== 'cat-pic') { return; }
       model.currentCat.clicks++;
       currentCatView.changeDisplayedClicks();
+    });
+  },
+  adminButtonListener() {
+    document.querySelector('#admin-button').addEventListener('click', () => {
+      catAdminView.toggle();
+    });
+  },
+  saveButtonListener() {
+    document.querySelector('#save-button').addEventListener('click', () => {
+      let currentCat = this.getCurrentCat();
+
+      currentCat.name = document.querySelector('#new-name').value;
+      currentCat.src = document.querySelector('#new-pic-url').value;
+      currentCat.clicks = document.querySelector('#new-click-count').value;
+
+      currentCatView.render();
+      catListView.update();
+      catAdminView.toggle();
+    });
+  },
+  cancelButtonListener() {
+    document.querySelector('#cancel-button').addEventListener('click', () => {
+      catAdminView.toggle();
     });
   }
 };
@@ -55,33 +78,70 @@ const catListView = {
       let catNameText = document.createTextNode(`${cat.name}`);
       listFragment.appendChild(catNameContainer).appendChild(catNameText);
     });
-    
-    document.querySelector('aside').appendChild(listFragment);
+
+    document.querySelector('#cat-list').appendChild(listFragment);
   },
   highlightSelectedCat(currentCatListEntry) {
-    document.querySelectorAll('aside > p').forEach((p) => {
+    document.querySelectorAll('#cat-list > p').forEach((p) => {
       p.style.background = '';
       p.style.color = 'white';
     });
     currentCatListEntry.style.background = 'white';
     currentCatListEntry.style.color = '#191c57';
+  },
+  update() {
+    let theCats = octopus.getCats();
+    let listEntries = document.querySelectorAll('#cat-list > p');
+
+    theCats.forEach(cat => {
+      listEntries[theCats.indexOf(cat)].innerText = cat.name; 
+    });
+  }
+};
+
+const catAdminView = {
+  toggle() {
+    let adminView = document.querySelector('#admin');
+    let listView = document.querySelector('#cat-list');
+
+    if (octopus.getCurrentCat().name === 'Start Page') {return;}
+
+    if (adminView.style.display == 'block') {
+      adminView.style.display = 'none';
+      listView.style.display = 'block';
+    } else { 
+      this.fillValues();
+      adminView.style.display = 'block';
+      listView.style.display = 'none';
+    }
+  },
+  fillValues() {
+    let currentCat = octopus.getCurrentCat();
+    if (octopus.getCurrentCat().name === 'Start Page') {return;}
+    document.querySelector('#new-name').value = currentCat.name;
+    document.querySelector('#new-pic-url').value = currentCat.src;
+    document.querySelector('#new-click-count').value = currentCat.clicks;
   }
 };
 
 const currentCatView = {
   render(firstRender) {
     let currentCat = octopus.getCurrentCat();
-    document.querySelector('#the-cat-pic > img').src = currentCat.src;
-    document.querySelector('#the-cat-pic > img').alt = currentCat.name;
+    let catPic = document.querySelector('#cat-pic');
+    let clickCount = document.querySelector('#click-count');
+
+    catPic.src = currentCat.src;
+    catPic.alt = currentCat.name;
     if (!firstRender) {
-      document.querySelector('#count').style.visibility = 'visible';
-      document.querySelector('#the-cat-pic > img').style.border = '2px solid white';
-      document.querySelector('#the-cat-pic > #count').innerText = `Clicks: ${currentCat.clicks}`;
+      catPic.style.border = '2px solid white';
+      clickCount.style.visibility = 'visible';
+      clickCount.innerText = `Clicks: ${currentCat.clicks}`;
     }
   },
   changeDisplayedClicks() {
     let currentCat = octopus.getCurrentCat();
-    document.querySelector('#the-cat-pic > #count').innerText = `Clicks: ${currentCat.clicks}`;
+    document.querySelector('#click-count').innerText = `Clicks: ${currentCat.clicks}`;
+    document.querySelector('#new-click-count').value = currentCat.clicks;
   }
 };
 
